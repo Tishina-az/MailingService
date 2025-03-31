f8b6f92741cb071aea3126f093a8d25df31b30c8
Andrey Zashchepin <razlivnoy@icloud.com>
2025-03-31 18:34:31 +0500

    Модель CustomUser

--- config/settings.py
+++ config/settings.py
@@ -103,12 +103,12 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = 'users.CustomUser'
---
+++ users/urls.py
@@ -0,0 +1,9 @@
+# from django.contrib.auth.views import LogoutView
+# from django.urls import path
+
+
+app_name = 'users'
+
+urlpatterns = [
+
+]
